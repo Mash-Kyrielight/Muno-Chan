ff684fb997e25f92078f20398c3fc9ee7a74bc99
shimotuki <yamashita@s15.yeskist.net>
2016-07-11 10:16:14 +0900

    no message

---
+++ Index.php
@@ -0,0 +1,11 @@
+<!DOCTYOE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<body>
+		<form action='DB_Connect.php' method='POST'>
+			<input type="text" name="Word" />
+			<input type="submit" value="聞く" />
+		</form>
+	</body>
+</html>
--- README.md
+++
@@ -1,1 +0,0 @@
-# Muno-Chan
