1703085dfb8ffacd3f3c591794cd3db0ea57cd64
Takumi Morisawa <taku1996mo@yahoo.co.jp>
2016-07-26 09:35:05 +0900

    スタイル適用のためタグ追加

--- dict.php
+++ dict.php
@@ -3,20 +3,29 @@
 <html lang="ja">
 <head>
 <meta charset="UTF-8">
+<link rel="stylesheet" type="text/css" href="css/reset.css">
+<link rel="stylesheet" type="text/css" href="css/style.css">
 <title>会話プログラム | 登録完了</title>
-<script src="http://code.jquery.com/jquery-3.0.0.min.js
-"></script>
+<script src="http://code.jquery.com/jquery-3.0.0.min.js">
+</script>
 <script src="script.js"></script>
 </head>
 <body>
-
-<p>会話例を教えてくれてありがとう！</p>
+<header>
+	<h1>Muno-Chan</h1>
+</header>
+<br><br><br>
+<article>
+<p id="dict">会話例を教えてくれてありがとう！</p>
 
 <!-- 登録会話例をすべて表示 -->
-<p>登録されている会話集です</p>
-<?=dumpDict()?>
+<p id="dict2">登録されている会話集です</p>
+<p id="dict2"><?=dumpDict()?></p>
 
 <a href="index.php">会話へ戻る</a>
-
+</article>
+<footer>
+	<small>&copy; 2016 AI development Muno-Chan</small>
+</footer>
 </body>
 </html>
